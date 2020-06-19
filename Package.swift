754cde091b6053ca0b447afd5ecdcd4764d47c93
Geert Berkers <geertberkers@hotmail.com>
2020-06-19 13:13:52 +0200

    Added macOs 13 platform

--- Package.swift
+++ Package.swift
@@ -20,6 +20,9 @@ import PackageDescription
 
 let package = Package(
     name: "Kitura-Session-Redis",
+    platforms: [
+        .macOS(.v10_13),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(

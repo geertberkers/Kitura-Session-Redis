8e40de45a3543f423478b67759a430ed3c2a13dd
Geert Berkers <geertberkers@hotmail.com>
2020-07-05 04:18:28 +0200

    Added missing extension

--- Sources/KituraSessionRedis/RedisStore.swift
+++ Sources/KituraSessionRedis/RedisStore.swift
@@ -263,3 +263,17 @@ public class RedisStore: Store {
     private typealias APICallback = (Data?, NSError?) -> Void
 
 }
+
+extension Data {
+
+    init<T>(from value: T) {
+        self = Swift.withUnsafeBytes(of: value) { Data($0) }
+    }
+
+    func to<T>(type: T.Type) -> T? where T: ExpressibleByIntegerLiteral {
+        var value: T = 0
+        guard count >= MemoryLayout.size(ofValue: value) else { return nil }
+        _ = Swift.withUnsafeMutableBytes(of: &value, { copyBytes(to: $0)} )
+        return value
+    }
+}
